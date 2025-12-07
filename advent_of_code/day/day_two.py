57dc4917aae31fa3a9b82aff08834f23b62fe60c
Michael Pollock (home pc) <originalmikep@gmail.com>
2025-12-07 17:41:35 -0500

    started day two pt two. WIP. Tests not all passing. Something funky goin on.

--- advent_of_code/day/day_two.py
+++ advent_of_code/day/day_two.py
@@ -1,23 +1,24 @@
-from advent_of_code.helper_files.io_operations import get_input
+from advent_of_code.helper_files.io_operations import get_csv_input
 
 
 def day_two_main(): 
-    input = get_input(file_path = 'inputs/day_two_input.csv')
-    print(f'input: {input}')
+    input = get_csv_input(file_path = 'inputs/day_two_input.csv')
+    invalid_ids = get_repeating_id_halfs(input)
+    print(f'Day two part one: sum of invalid ids: {sum(invalid_ids)}')
+    invalid_ids = get_repeating_id_sections(input)
+    print(f'Day two part two: sum of invalid ids: {sum(invalid_ids)}')
 
-def get_invalid_ids(data_ranges: list[str]):
+def get_repeating_id_halfs(data_ranges: list[str]):
     invalid_ids = []
-    print(f'data_ranges: {data_ranges}')
     for range in data_ranges:
         range_bounds = range.split('-')
         if len(range_bounds) != 2:
             continue
-        invalid_ids.extend(get_invalid_ids_by_range(start=int(range_bounds[0]), end=int(range_bounds[1])))
+        invalid_ids.extend(get_repeating_id_halfs_by_range(start=int(range_bounds[0]), end=int(range_bounds[1])))
 
     return invalid_ids
 
-def get_invalid_ids_by_range(start: int, end: int):
-    print(f'getting invalid ids for range {start} - {end}')
+def get_repeating_id_halfs_by_range(start: int, end: int):
     if start > end:
         temp_start = start
         start = end
@@ -28,8 +29,7 @@ def get_invalid_ids_by_range(start: int, end: int):
     if len(start_str) == len(end_str) and len(start_str) % 2 != 0:
         return []
     if len(start_str) == len(end_str) and len(start_str) % 2 == 0:
-        invalid_ids = list(int(id) for id in range(start, end + 1) if not id_is_valid(id))
-        print(f'invalid ids: {invalid_ids}')
+        invalid_ids = list(int(id) for id in range(start, end + 1) if not id_halfs_are_unique(id))
         return invalid_ids
     invalid_ids = []
     id:int = start
@@ -37,16 +37,13 @@ def get_invalid_ids_by_range(start: int, end: int):
     while id <= end:
         id_str = str(id)
         if len(id_str) % 2 != 0:
-            new_id_str = '1' + '0'*len(id_str)
-            print(f'jumping from {id_str} to {new_id_str}')
-            id = int(new_id_str)
+            id = int('1' + '0'*len(id_str))
             continue
-        if not id_is_valid(id): invalid_ids.append(id)
+        if not id_halfs_are_unique(id): invalid_ids.append(id)
         id = id + 1
-    print(f'returning {invalid_ids}')
     return invalid_ids
 
-def id_is_valid(id: int):
+def id_halfs_are_unique(id: int):
     id_str = str(id)
     if len(id_str)%2 != 0:
         return True
@@ -54,3 +51,50 @@ def id_is_valid(id: int):
     left = id_str[:center_index]
     right = id_str[center_index:]
     return left != right 
+
+def id_sections_are_unique(id: int):
+    print(f'running for {id}')
+    id_str = str(id)
+    section_lengths = []
+    for i in range(len(id_str) // 2 + 1):
+        if i < 1: continue
+        if len(id_str) % i != 0: continue
+        section_lengths.append(i)
+    section_lengths.sort(reverse=True)
+    print(f'possible section lengths: {section_lengths}')
+    for length in section_lengths:
+        sections = []
+        prev_section = None
+        prev_section_matches = False
+        for i in range(0, len(id_str), length):
+            sections.append(id_str[i:i+length])
+            if prev_section != id_str[i:i+length]:
+                prev_section = id_str[i:i+length]
+                continue
+            prev_section_matches = True
+        print(f'sections: {sections}')
+        if prev_section_matches: 
+            return False
+    return True
+
+def get_repeating_id_sections_by_range(start: int, end: int):
+    if start > end:
+        temp_start = start
+        start = end
+        end = temp_start
+    invalid_ids = []
+    id: int = start
+    while id <= end:
+        if not id_sections_are_unique(id): invalid_ids.append(id)
+        id += 1
+    return invalid_ids
+
+def get_repeating_id_sections(data_ranges: list[str]):
+    invalid_ids = []
+    for range in data_ranges:
+        range_bounds = range.split('-')
+        if len(range_bounds) != 2:
+            continue
+        invalid_ids.extend(get_repeating_id_sections_by_range(start=int(range_bounds[0]), end=int(range_bounds[1])))
+
+    return invalid_ids
--- test_advent_of_code/test_day_two.py
+++ test_advent_of_code/test_day_two.py
@@ -1,37 +1,36 @@
 from unittest import TestCase
 import os
-from advent_of_code.day.day_two import get_invalid_ids, get_invalid_ids_by_range, id_is_valid
+from advent_of_code.day.day_two import get_repeating_id_halfs, get_repeating_id_halfs_by_range, \
+    get_repeating_id_sections, get_repeating_id_sections_by_range, id_halfs_are_unique, id_sections_are_unique
 from advent_of_code.helper_files.io_operations import get_csv_input
 
 
 class DayTwoTests(TestCase):
 
-    def test_id_is_valid(self):
-        self.assertTrue(id_is_valid(21))
-        self.assertFalse(id_is_valid(22))
-        self.assertTrue(id_is_valid(121))
-        self.assertFalse(id_is_valid(2121))
-        self.assertFalse(id_is_valid(21032103))
-        self.assertTrue(id_is_valid(1234567123456))
+    def test_id_halfs_are_unique(self):
+        self.assertTrue(id_halfs_are_unique(21))
+        self.assertFalse(id_halfs_are_unique(22))
+        self.assertTrue(id_halfs_are_unique(121))
+        self.assertFalse(id_halfs_are_unique(2121))
+        self.assertFalse(id_halfs_are_unique(21032103))
+        self.assertTrue(id_halfs_are_unique(1234567123456))
 
-    def test_get_invalids_by_range(self):
-        self.assertListEqual([11, 22], get_invalid_ids_by_range(start=11, end=22))
+    def test_get_repeating_id_halfs_by_range(self):
+        self.assertListEqual([11, 22], get_repeating_id_halfs_by_range(start=11, end=22))
 
-    def test_get_invalid_ids_11_through_1010(self):
-        dir_path = os.path.join(os.path.dirname(__file__), 'test_helpers')
-        file_path = os.path.join(dir_path, 'day_two_test_input.csv')
+    def test_get_repeating_id_halfs_11_through_1010(self):
         input_data = ['11-1010']
-        invalid_ids = get_invalid_ids(input_data)
+        invalid_ids = get_repeating_id_halfs(input_data)
         expected_invalid_ids = [
             11, 22, 33, 44, 55, 66, 77, 88, 99, 1010
         ]
         self.assertEqual(expected_invalid_ids, invalid_ids)
 
-    def test_get_invalid_ids(self):
+    def test_get_repeating_id_halfs(self):
         dir_path = os.path.join(os.path.dirname(__file__), 'test_helpers')
         file_path = os.path.join(dir_path, 'day_two_test_input.csv')
         input_data = get_csv_input(file_path)
-        invalid_ids = get_invalid_ids(input_data)
+        invalid_ids = get_repeating_id_halfs(input_data)
         expected_invalid_ids = [
             11, 22, 99, 1010, 1188511885, 222222, 446446, 38593859
         ]
@@ -43,3 +42,33 @@ class DayTwoTests(TestCase):
         ]
         id_sum = sum(invalid_ids)
         self.assertEqual(1227775554, id_sum)
+
+    def test_id_sections_are_unique(self):
+        self.assertTrue(id_sections_are_unique(21))
+        self.assertFalse(id_sections_are_unique(22))
+        self.assertTrue(id_sections_are_unique(121))
+        self.assertFalse(id_sections_are_unique(2121))
+        self.assertFalse(id_sections_are_unique(21032103))
+        self.assertTrue(id_sections_are_unique(1234567123456))
+
+    def test_get_repeating_id_sections_by_range(self):
+        self.assertListEqual([11, 22], get_repeating_id_sections_by_range(start=11, end=22))
+
+    def test_get_repeating_id_sections_11_through_1010(self):
+        input_data = ['11-1010']
+        invalid_ids = get_repeating_id_sections(input_data)
+        expected_invalid_ids = [
+            11, 22, 33, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999, 1010
+        ]
+        self.assertEqual(expected_invalid_ids, invalid_ids)
+
+    def test_get_repeating_id_sections(self):
+        dir_path = os.path.join(os.path.dirname(__file__), 'test_helpers')
+        file_path = os.path.join(dir_path, 'day_two_test_input.csv')
+        input_data = get_csv_input(file_path)
+        invalid_ids = get_repeating_id_sections(input_data)
+        expected_invalid_ids = [
+            11, 22, 99, 1010, 1188511885, 222222, 446446, 38593859
+        ]
+        self.assertEqual(expected_invalid_ids, invalid_ids)
+
