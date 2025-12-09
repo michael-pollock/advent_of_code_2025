01d225504e0e81445bd7397a65a97de53c79354f
Michael Pollock (home pc) <originalmikep@gmail.com>
2025-12-09 17:01:55 -0500

    tiny bit o progress

--- advent_of_code/day/day_4.py
+++ advent_of_code/day/day_4.py
@@ -8,3 +8,10 @@ def day_4_main():
 def init_input(file_path):
     input = get_input(file_path = file_path)
     return input
+
+def get_num_of_surrounding_rolls(row_index: int, col_index: int, grid: list[list[str]]):
+    print(f'current location: [{row_index}][{col_index}]: {grid[row_index][col_index]}')
+    surrounding_indicies = []
+    row_check = row_index - 1
+    col_check = col_index - 1
+
